9d5fa1e6cce44218467c94141df262cf588d8875
Shayne Johnson <shayne@eventbase.com>
2018-12-04 16:26:52 -0800

    Converted Function (RandInt) to ES6 Syntax

--- cypress/integration/giphySuite.js
+++ cypress/integration/giphySuite.js
@@ -19,7 +19,7 @@ As a signed in user, I can successfully log out
 
 // Generate random integer
 
-function getRandomInt (min, max) {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
