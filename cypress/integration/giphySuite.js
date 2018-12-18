19fca9921664386634b549d857414376498e1029
Shayne Johnson <shayne@eventbase.com>
2018-12-18 14:15:06 -0800

    Test is now complete Timers may be excessive but it needs to account for poor internet connections

--- cypress/integration/giphySuite.js
+++ cypress/integration/giphySuite.js
@@ -30,7 +30,7 @@ const randInt = getRandomInt(1, 6000)
 const uploadImg = 'https://www.youtube.com/watch?v=a1Y73sPHKxw'
 
 describe('Assertions', () => {
-  it.skip('Invalid Email Test', () => {
+  it('Invalid Email Test', () => {
     cy.visit('https://giphy.com/')
     cy.get('#login-button').click()
     cy.get(':nth-child(7) > a').click()
@@ -39,7 +39,7 @@ describe('Assertions', () => {
     cy.get('.ss-alert').should('be.visible')
   })
 
-  it('User can create a new acount', () => {
+  it('User can create a new account, favorite and upload', () => {
     cy.visit('https://giphy.com/')
     cy.get('#login-button').click()
     cy.get(':nth-child(7) > a').click()
@@ -53,17 +53,20 @@ describe('Assertions', () => {
     cy.url().should('eq', 'https://giphy.com/search/Canada')
     cy.get('img[alt="canada GIF"]').first().click()
 
-    // click the favorite button on the gif
+    // click the favorite button on the gif 
+    // Note: Element has no human readable name, so generated class names are used.
     cy.get('._2T3VM71zjFUfWcC0wxlqte > ._3_Iqv2eF8FuK3joSjURmOO').click()
 
     // View new favourites and unfavourite the Canada gif
     cy.wait(5000)
     cy.visit('https://giphy.com/favorites')
 
-    // put assertion to verify page is not empty
+    
     cy.get('.gif_gifImage__3dFjs').first().click()
     cy.get('._1_cwJiJLy366TYHJ0ZojUn').click()
-    // put assertion to verify page is now empty
+    // put assertion to verify page is empty after removing favorite
+    cy.wait(5000)
+    cy.visit('https://giphy.com/favorites').get('a').contains('kitten').should('be.visible')
 
     // Open Settings
     cy.wait(5000)
@@ -75,37 +78,40 @@ describe('Assertions', () => {
         expect(response.status).to.eq(200)
         cy.get('input[name="is_public"]').should('have.value', 'false')
       })
-  })
+    
 
-  it('Determine image uploader works', () => {
-    cy.visit('https://giphy.com/')
-
-    // create button
-    cy.get('._23RtopKAI5Hj6SZSgi0wR5').click()
-    cy.url().should('eq', 'https://giphy.com/create/gifmaker')
-
-    // upload Youtube link
-    cy.get('._35BEC7IGiS5fzfdaPB86jq').click().type(uploadImg)
-    cy.url().should('eq', 'https://giphy.com/create/gifmaker/video-trim')
-
-    // begin upload after trim page
-    cy.get('._6WZyciPmD3H_QZtPwGaAq').click()
-    cy.url().should('eq', 'https://giphy.com/create/gifmaker/decorate/caption')
-    cy.wait(5000)
-
-    // close the ribbon
-    cy.get('.flash-message__MessageClose-grey4k-3').click()
-
-    // Add caption and confirm upload
-    cy.get('._3hPx2FPos5hh7ZJE4tRUkI').click().type('FQA test test')
-    cy.wait(5000)
-    cy.get(':nth-child(2) > ._6WZyciPmD3H_QZtPwGaAq').click()
-
-    // final upload confirmation
-    cy.get('div').contains('Start Over').siblings('div').click()
-
-    // Logout
-    cy.wait(7500)
-    cy.visit('https://www.giphy.com/logout')
-  })
+      cy.visit('https://giphy.com/')
+  
+      // create button
+      cy.get('._23RtopKAI5Hj6SZSgi0wR5').click()
+      cy.url().should('eq', 'https://giphy.com/create/gifmaker')
+  
+      // upload Youtube link
+      cy.get('._35BEC7IGiS5fzfdaPB86jq').click().type(uploadImg)
+      cy.url().should('eq', 'https://giphy.com/create/gifmaker/video-trim')
+  
+      // begin upload after trim page
+      cy.get('._6WZyciPmD3H_QZtPwGaAq').click()
+      cy.url().should('eq', 'https://giphy.com/create/gifmaker/decorate/caption')
+      cy.wait(5000)
+  
+      // close the ribbon
+      cy.get('.flash-message__MessageClose-grey4k-3').click()
+  
+      // Add caption and confirm upload
+      cy.get('._3hPx2FPos5hh7ZJE4tRUkI').click().type('FQA test test')
+      cy.wait(5000)
+      cy.get(':nth-child(2) > ._6WZyciPmD3H_QZtPwGaAq').click()
+  
+      // final upload confirmation
+      cy.get('div').contains('Start Over').siblings('div').click()
+  
+      // Logout
+      cy.wait(10000)
+      cy.get('._17hY_dW7917mzr2S9Ree').click()
+      cy.reload()
+      cy.get('.gif_gifImage__3dFjs').should('be.visible')
+      cy.visit('https://www.giphy.com/logout')
+    })
+    
 })
